c0637bc7d0b4646bb8666e2d75b6ea7798ed326c
gustavooors <140653194+gustavooors@users.noreply.github.com>
2023-08-07 16:47:09 -0300

    adicionando arquivos js e css

---
+++ sketch.js
@@ -0,0 +1,19 @@
+function setup() {
+    createCanvas(500, 400);
+   }
+  
+  function draw() {
+    background(imagemdaestrada);
+     mostraator();
+     mostracarro();
+     movimentacarro();
+     movimentaator();
+     voltaposicaoinicial();
+     verificacolisao();
+     incluipontos();
+     marcaponto();
+  }
+  
+  
+  
+  
